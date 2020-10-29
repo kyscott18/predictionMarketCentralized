f702630ebdde5289c56a8cf24dc804e1ae1db0bd
kyscott <kyscott@umich.edu>
2020-10-29 14:59:41 -0400

    command line arguments

--- main.go
+++ main.go
@@ -1,46 +1,63 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+
+	"example.com/predictionMarketCentralized/maker"
 	"example.com/predictionMarketCentralized/markets"
 	"example.com/predictionMarketCentralized/players"
+	"example.com/predictionMarketCentralized/simulatedPlayer"
 )
 
 func main() {
-	cs := markets.NewContractSet("coin flip", []string{"heads", "tails"}, []float32{.5, .5}, 20)
-	mp1 := players.NewMarketPlayer(1, 10)
-	mp1.PrintState()
-	cs.PrintState()
-	mp1.BuyContract(&cs, &cs.Markets[0], 2)
-	mp1.PrintState()
-	cs.PrintState()
-	mp1.AddLiquidity(&cs, &cs.Markets[0], 1.5)
-	mp1.PrintState()
-	cs.PrintState()
-	// mm.Make(&cs)
-	// mm.PrintState()
-	// cs.PrintState()
-	// mp1.SellContract(&cs, &cs.Markets[0], 5)
-	// mp1.PrintState()
-	// cs.PrintState()
-	// mm.Make(&cs)
-	// mm.PrintState()
-	// cs.PrintState()
+	typePtr := flag.String("type", "basic", "basic or simulated")
+	verbosePtr := flag.Bool("v", false, "a bool")
+	flag.Parse()
 
-	//cs := markets.NewContractSet("coin flip", []string{"heads", "tails"}, []float32{.5, .5}, 200)
-	//mm := maker.NewMarketMaker()
-	// bots := make([]simulatedPlayer.SimulatedPlayer, 0)
-	// for i := 0; i < 100; i++ {
-	// 	bots = append(bots, simulatedPlayer.NewSimulatedPlayer(i, 70))
-	// }
-	// for round := 0; round < 800; round++ {
-	// 	for i := 0; i < 100; i++ {
-	// 		for j := 0; j < len(cs.Markets); j++ {
-	// 			bots[i].Take(&cs, &cs.Markets[j])
-	// 			mm.Make(&cs)
-	// 		}
-	// 	}
-	// }
-	// mm.PrintState()
+	if *verbosePtr {
+		fmt.Println("Verbose")
+	} else {
+		fmt.Println("NotVerbose")
+	}
+	if *typePtr == "basic" {
+		cs := markets.NewContractSet("coin flip", []string{"heads", "tails"}, []float32{.5, .5}, 20)
+		mp1 := players.NewMarketPlayer(1, 10)
+		mp1.PrintState()
+		cs.PrintState()
+		mp1.BuyContract(&cs, &cs.Markets[0], 2)
+		mp1.PrintState()
+		cs.PrintState()
+		mp1.AddLiquidity(&cs, &cs.Markets[0], 1.5)
+		mp1.PrintState()
+		cs.PrintState()
+		// mm.Make(&cs)
+		// mm.PrintState()
+		// cs.PrintState()
+		// mp1.SellContract(&cs, &cs.Markets[0], 5)
+		// mp1.PrintState()
+		// cs.PrintState()
+		// mm.Make(&cs)
+		// mm.PrintState()
+		// cs.PrintState()
+	} else if *typePtr == "simulated" {
+		cs := markets.NewContractSet("coin flip", []string{"heads", "tails"}, []float32{.5, .5}, 200)
+		mm := maker.NewMarketMaker()
+		bots := make([]simulatedPlayer.SimulatedPlayer, 0)
+		for i := 0; i < 100; i++ {
+			bots = append(bots, simulatedPlayer.NewSimulatedPlayer(i, 70))
+		}
+		for round := 0; round < 800; round++ {
+			for i := 0; i < 100; i++ {
+				for j := 0; j < len(cs.Markets); j++ {
+					bots[i].Take(&cs, &cs.Markets[j])
+					mm.Make(&cs)
+				}
+			}
+		}
+		mm.PrintState()
 
-	// cs.PrintState()
+		cs.PrintState()
+
+	}
 }
